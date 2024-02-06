bf0ca6844a8508d82f59508d07255cd87c7c18c8
pinnekermv <pinnekermv@gmail.com>
2024-02-06 16:36:55 +0200

    редактирование Типов событий

---
+++ src/renderer/components/dialog/EventTypeEditDialog.js
@@ -0,0 +1,79 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  Button,
+  Intent,
+} from '@blueprintjs/core';
+
+import EventTypeForm from '../form/event/EventTypeForm';
+import EventTypeCategoryPropType from '../../props/EventTypeCategoryPropType';
+import EventType from '../eventType/EventType';
+
+export default function EventTypeEditDialog({
+  callback,
+  categoryCreateCallback,
+  etDisplay,
+  categories,
+  isOpen,
+  toggle,
+  eventType
+}) {
+  const ref = useRef();
+
+  const passState = () => {
+    callback(ref.current.getFormData(), toggle);
+  };
+
+  const handleNewCategory = (name, color) => {
+    categoryCreateCallback(name, color);
+  };
+
+  // build root cats
+  const cats = categories
+    .filter((c) => c.parent === 0)
+    .map((c) => ({ id: c.id, name: etDisplay.getCategory(c) }));
+
+  return (
+    <Dialog
+      isOpen={isOpen}
+      onClose={toggle}
+      canOutsideClickClose={false}
+      title="Edit event type"
+    >
+      <DialogBody>
+        <EventTypeForm
+          eventType={eventType}
+          categories={cats}
+          ref={ref}
+          categoryCreateCallback={handleNewCategory}
+        />
+      </DialogBody>
+      <DialogFooter
+        minimal
+        actions={
+          <Button intent={Intent.PRIMARY} onClick={passState} text="Update" />
+        }
+      />
+    </Dialog>
+  );
+}
+
+EventTypeEditDialog.propTypes = {
+  etDisplay: PropTypes.objectOf(EventType).isRequired,
+  callback: PropTypes.func.isRequired,
+  categoryCreateCallback: PropTypes.func,
+  categories: PropTypes.arrayOf(EventTypeCategoryPropType),
+  isOpen: PropTypes.bool,
+  toggle: PropTypes.func,
+};
+
+EventTypeEditDialog.defaultProps = {
+  eventType: null,
+  categories: [],
+  isOpen: false,
+  toggle: () => {},
+  categoryCreateCallback: () => {},
+};
